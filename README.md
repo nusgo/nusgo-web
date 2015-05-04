558a7d7c2c9aa01d869457a81c6b8271f7416167
Andhieka Putra <andhieka.putra@gmail.com>
2015-05-05 01:32:29 +0800

    Project scaffolding

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nusgo-web
 NUSGo Web Version. Implemented using NodeJS backend and HTML-CSS-JS front end.
+
+## Installation
+
+First, you need to download [NodeJS](https://nodejs.org/download/).
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('port', (process.env.PORT) || 5000);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+});
+
+http.listen(process.env.PORT || 5000, function() {
+    console.log('NusGo Server is ready to serve on port ' + app.get('port') + "!");
+});
+
+

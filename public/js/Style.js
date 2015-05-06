b14a293c11b149d750b73124d124909839ac0469
ipeech <i.peech@gmail.com>
2015-05-07 06:49:09 +0800

    Experiment with full-width map to tie in with modern map app look - Still a lot of bugs with this new layout - Pane on the side could be revamped to be a pop up and simplified - To KS and Andieka: If you don't like the direction this is heading, feel   free to revert style.css and index.html to previous commit

--- public/js/Style.js
+++ public/js/Style.js
@@ -1,5 +1,9 @@
+//
+
 // WRITE FUNCTION DEFINITIONS HERE
 
+var heightMap;
+var heightMapString;
 
 // Call this function everytime chatArea is updated.
 // 1. Each time user sends a chat message
@@ -12,6 +16,38 @@ function scrollChatAreaToLatest(){
 
 
 
+function setMapSize() {
+	heightMap = $(window).height() - 100;
+
+	heightMapString = "height:"+heightMap+"px";
+
+	//document.getElementById("googleMap").setAttribute("style",heightMapString);
+	document.getElementById("googleMap").setAttribute("style","height:200px");
+
+
+
+}
+
+function resize()
+{
+ //alert("resize: setting map size...");
+ //setMapSize();
+}
+
+
+
+window.setInterval(function(){
+  resize();
+}, 5000);
+
+//setMapSize();
+
+//while(1) {
+	
+//}
+//window.setInterval(resize(),2000);
 
 //Call Funtions Here
-//setInterval(scrollChatAreaToLatest,1000);
+//setMapSize();
+
+//window.onresize = resize;

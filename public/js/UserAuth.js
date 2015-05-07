dcfc41f59d611cee6a4a32171382a71cf7974e49
ks-uppercut <tankangsoon@gmail.com>
2015-05-07 08:28:59 +0800

    Remove "Please log into facebook" message when user is not logged in. Added "col-sm" to home desc overlay

--- public/js/UserAuth.js
+++ public/js/UserAuth.js
@@ -17,14 +17,14 @@ UserAuth.prototype.fbStatusChangeCallback = function(response) {
     } else if (response.status === 'not_authorized') {
         // The person is logged into Facebook, but not your app.
         this.isLogin = false;
-        document.getElementById('status').innerHTML = 'Please log ' +
-            'into this app.';
+        /*document.getElementById('status').innerHTML = 'Please log ' +
+            'into this app.';*/
     } else {
         // The person is not logged into Facebook, so we're not sure if
         // they are logged into this app or not.
         this.isLogin = false;
-        document.getElementById('status').innerHTML = 'Please log ' +
-            'into Facebook.';
+        /*document.getElementById('status').innerHTML = 'Please log ' +
+            'into Facebook.';*/
     }
 };
 
